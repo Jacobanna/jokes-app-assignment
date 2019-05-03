e65f2e6db47ab8c77d0e9b32d21849370c4f85df
Jacobanna <pieprzyk.jakub@gmail.com>
2019-05-03 16:06:15 +0200

    Java bean configuration and XML bean configuration examples.

---
+++ src/main/java/guru/springframework/config/ChuckConfiguration.java
@@ -0,0 +1,11 @@
+package guru.springframework.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+//@Configuration
+public class ChuckConfiguration {
+//    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
